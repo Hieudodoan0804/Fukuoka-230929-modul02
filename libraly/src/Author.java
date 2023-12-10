c2f74761cbaa3118beba62ae7dea3a61a559ff3f
DOAN HIEU <user@DO-DOAN-HIEU.local>
2023-12-11 00:17:20 +0900

    add

--- libraly/src/Author.java
+++ libraly/src/Author.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class Author {
+import java.util.Scanner;
+
+public class Author {
+    private String id;
+    private String name;
+    private String email;
+    private String gender;
+    private int age;
+    private Author[] authors;
+
+    public Author() {
+    }
+
+    public Author(String id, String name, String email, String gender, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addAuthor() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập id tác giả:");
+        id =scanner.nextLine();
+        System.out.println("Nhập tên tác giả:");
+        name = scanner.nextLine();
+        System.out.println("Nhập email tác giả:");
+        email = scanner.nextLine();
+        System.out.println("Nhập gender tác giả:");
+        gender = scanner.nextLine();
+        System.out.println("Nhập age tác giả:");
+        age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Tác giả đã được thêm thành công.");
+    }
+
+
+
+
 }
--- libraly/src/Book.java
+++ libraly/src/Book.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Book {
+public class Book {
+    private int id;
+    private String title;
+    private Author author;
+    private double price;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, Author author, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
 }
